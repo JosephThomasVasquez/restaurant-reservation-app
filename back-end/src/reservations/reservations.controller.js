b69caa9dda4af30c5df6e1f1accee10caa47d316
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-01-30 16:36:24 -0800

    Set valid date to string

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -77,7 +77,7 @@ const hasDate = (req, res, next) => {
 
 // Checks if reservation_date is a valid date
 const hasValidDate = (req, res, next) => {
-  let isValid = new Date(res.locals.reservation_date).toUTCString();
+  let isValid = new Date(res.locals.reservation_date).toString();
 
   if (isValid === "Invalid Date") {
     return next({
